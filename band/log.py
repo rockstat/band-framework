ae9ec7b04bcdcbca25e2113a036864bf02ac0ae2
Dmitry Rodin <madiedinro@gmail.com>
2018-05-22 09:56:03 +0300

    color and json logs

--- band/log.py
+++ band/log.py
@@ -1,15 +1,15 @@
 import logging
-# import coloredlogs
+import sys
+import coloredlogs
 
 LOGGER_FORMAT = '%(asctime)s %(levelname)s %(message)s'
 JSON_FORMAT='{ "loggerName":"%(name)s", "asciTime":"%(asctime)s", "fileName":"%(filename)s", "logRecordCreationTime":"%(created)f", "functionName":"%(funcName)s", "levelNo":"%(levelno)s", "lineNo":"%(lineno)d", "time":"%(msecs)d", "levelName":"%(levelname)s", "message":"%(message)s"}'
-# coloredlogs.install()
 
-# , datefmt='[%H:%M:%S]'
-logging.basicConfig(format=JSON_FORMAT)
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', fmt=JSON_FORMAT, stream=sys.stdout)
 
+# logger.setLevel(logging.DEBUG)
+# logging.basicConfig(, datefmt='[%H:%M:%S]')
 
 __all__ = ['logger']
