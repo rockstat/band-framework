f8f5e1682c7e463400e6fae5186dca4acec098e9
Dmitry Rodin <madiedinro@gmail.com>
2018-06-18 22:52:41 +0300

    error handler

--- band/server.py
+++ band/server.py
@@ -15,7 +15,27 @@ https://git.duniter.org/clients/python/sakia/blob/master/src/sakia/main.py
 
 @web.middleware
 async def middleware(request, handler):
+    """
+    Simple CORS middleware to access api from dashboard
+    """
     return say_cors_yes(request) if request.method == 'OPTIONS' else await handler(request)
+
+# @web.middleware
+async def error_middleware(request, handler):
+    """
+    Error handling middleware
+    """
+    try:
+        response = await handler(request)
+        if response.status != 404:
+            return response
+        message = response.message
+    except web.HTTPException as ex:
+        if ex.status != 404:
+            raise
+        message = ex.reason
+    return web.json_response({'error': message})
+
 
 
 def loop_exc(loop, context):
