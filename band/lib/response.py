6f032ce8ea111a9917fb9128f2a93452d28627fb
Dmitry Rodin <madiedinro@gmail.com>
2018-09-11 00:44:15 +0300

    better naming

--- band/lib/response.py
+++ band/lib/response.py
@@ -5,15 +5,15 @@ RESP_REDIRECT = 'redirect'
 RESP_ERROR = 'error'
 
 
-def redirect(location, httpCode=302, data={}):
+def redirect_response(location, httpCode=302, data={}):
     return {RESP_META_KEY: dict(type=RESP_REDIRECT, location=location, httpCode=httpCode, data=data)}
 
 
-def pixel(data={}):
+def pixel_response(data={}):
     return {RESP_META_KEY: dict(type=RESP_PIXEL, data={})}
 
 
-def error(message="", httpCode=500, data={}):
+def error_response(message="", httpCode=500, data={}):
     return {RESP_META_KEY: dict(type=RESP_ERROR, errorMessage=message, httpCode=httpCode, data=data)}
 
 
