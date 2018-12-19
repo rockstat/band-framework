bed6474e07f87282883a422c5dbe09eb41c4c8f2
Dmitry Rodin <madiedinro@gmail.com>
2018-12-20 02:25:46 +0300

    custom config support

--- band/config/configfile.py
+++ band/config/configfile.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, FileSystemLoader, Template
-from collections import namedtuple
+import collections
+
 import os
 from pathlib import Path
 from pprint import pprint
@@ -12,16 +13,30 @@ from ..log import logger
 from ..constants import DIRECTOR_SERVICE
 from .env import ENV_DEV, ENV_PROD, env, name_env, environ
 
-CONFIG_FILE = 'config.yaml'
-CONFIG_DIR = '.'
+CONFIG_FILES = ['config.yaml', 'custom.yml']
+
+
+def update(d, u):
+    for k, v in u.items():
+        if isinstance(v, collections.Mapping):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
 
 try:
     root = Path(os.getcwd())
+    config = {}
     tmplenv = Environment(loader=FileSystemLoader(str(root)))
-    tmpl = tmplenv.get_template(CONFIG_FILE)
-    config = tmpl.render(**environ)
-    data = yaml.load(config)
-    data.update({
+    for fn in CONFIG_FILES:
+        if os.path.exists(fn):
+            tmpl = tmplenv.get_template(fn)
+            part = tmpl.render(**environ)
+            data = yaml.load(part)
+            update(config, data)
+    
+    
+    config.update({
         # use pre configured or detected service name
         'name': data.get('name', name_env),
         'env': env,
