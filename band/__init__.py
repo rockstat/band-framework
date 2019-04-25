efc08813e4647f55abaa6cdcad828fd9562cfa6c
Dmitry Rodin <madiedinro@gmail.com>
2019-04-25 12:30:09 +0300

    Bump version: 0.19.0 → 0.20.0

--- band/__init__.py
+++ band/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = '0.19.0'
+__VERSION__ = '0.20.0'
 
 import importlib
 import os
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-__VERSION__ = '0.19.0'
+__VERSION__ = '0.20.0'
 
 from setuptools import setup, find_packages
 
 setup(
     name='band',
-    version='0.19.0',
+    version='0.20.0',
     author='Dmitry Rodin',
     author_email='madiedinro@gmail.com',
     license='MIT',
